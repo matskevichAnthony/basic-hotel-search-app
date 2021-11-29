707b7e30213e98e7a04ad3158683fe6796ad3cc6
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2021-11-29 22:41:03 +0000

    API linked changed

--- src/store/sagas/hotels.js
+++ src/store/sagas/hotels.js
@@ -7,7 +7,7 @@ import { HOTELS_GET, HOTELS_SET } from '../constants';
 const today = dayjs().format('YYYY-MM-DD');
 const dayJsLocalized = dayjs.extend(localizedFormat);
 
-const API_URL = 'http://engine.hotellook.com/api/v2/';
+const API_URL = 'https://engine.hotellook.com/api/v2/';
 
 function* getHotels({ payload = {} }) {
     const { location = 'Москва', date = today, days = 1 } = payload;
@@ -15,7 +15,7 @@ function* getHotels({ payload = {} }) {
     const checkOut = dayjs(date).add(days, 'day').format('YYYY-MM-DD');
     const checkInTransformed = dayJsLocalized(date).locale(ru).format('LL');
 
-    const hotels = yield call(async() => {
+    const hotels = yield call(async () => {
         const response = await fetch(`${API_URL}cache.json?location=${location}&currency=rub&checkIn=${date}&checkOut=${checkOut}&limit=10&lang=ru`);
         return await response.json();
     });
