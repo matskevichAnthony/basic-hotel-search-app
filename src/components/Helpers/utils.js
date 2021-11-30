2d5a85d74d9cc7b43a99c3d54bb360fdf34e8c33
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2021-11-30 06:14:56 +0000

    Final refactoring

--- src/components/Helpers/utils.js
+++ src/components/Helpers/utils.js
@@ -15,23 +15,21 @@ export const getNoun = (number, one, two, five) => {
 };
 
 export const sortButtonMode = (e, state, setState) => {
+    console.log(state);
     console.log(e.target.children[0].children[0]);
-    if (state >= 2) {
-        setState(0)
+    if (state > 1) {
+        setState(1)
     } else {
         setState(state + 1);
     }
     if (state === 0) {
         e.target.children[0].children[0].style.color = '#E3E3E3';
-        e.target.children[0].children[1].style.color = '#E3E3E3';
+        e.target.children[0].children[1].style.color = 'black';
     } else if (state === 1) {
         e.target.children[0].children[1].style.color = 'black';
         e.target.children[0].children[0].style.color = '#E3E3E3';
     } else if (state === 2) {
         e.target.children[0].children[1].style.color = '#E3E3E3';
         e.target.children[0].children[0].style.color = 'black';
-    } else {
-        e.target.children[0].children[1].style.color = '#E3E3E3';
-        e.target.children[0].children[0].style.color = '#E3E3E3';
     }
 };
